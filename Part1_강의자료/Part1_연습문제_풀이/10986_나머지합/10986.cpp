0690be8f12528de248b28e253dbb765a4871f049
Sunwoo Song <sunwsong@c2r6s4.42seoul.kr>
2023-08-04 13:46:51 +0900

    upload: part1 practice 10986 codes(cpp, py)

---
+++ Part1_강의자료/Part1_연습문제_풀이/10986_나머지합/10986.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N, M;
+    scanf("%d %d", &N, &M);
+
+    vector<int> vec(N + 1);
+    for (int i = 1; i <= N; i++) {
+        int num;
+        scanf("%d", &num);
+        vec[i] = num % M;
+    }
+
+    vector<int> acc_remain_cnt(M);
+    int cur_remain = 0;
+    acc_remain_cnt[cur_remain]++;
+    long long ans = 0;
+
+    for (int i = 1; i <= N; i++) {
+        cur_remain = (cur_remain + vec[i]) % M;
+        ans += acc_remain_cnt[cur_remain];
+        acc_remain_cnt[cur_remain]++;
+    }
+    printf("%lld\n", ans);
+    return 0;
+}
---
+++ Part1_강의자료/Part1_연습문제_풀이/10986_나머지합/10986_map.cpp
@@ -0,0 +1,26 @@
+#include <map>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N, M;
+    scanf("%d %d", &N, &M);
+
+    int cur_remain = 0;
+    map<int, int> remain_cnt;
+    remain_cnt[cur_remain] = 1;
+    long long ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        int num;
+        scanf("%d", &num);
+        cur_remain = (cur_remain + num) % M;
+        ans += remain_cnt[cur_remain];
+        remain_cnt[cur_remain]++;
+    }
+
+    printf("%lld\n", ans);
+    return 0;
+}
